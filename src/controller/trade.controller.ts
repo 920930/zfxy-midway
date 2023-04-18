7a0b0826e7b54a2c51d0a6dfa3124cbf7c54429e
zhang <iyunx@qq.com>
2023-04-18 15:21:07 +0800

    feat: trade

--- src/controller/trade.controller.ts
+++ src/controller/trade.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Inject, Get, Param, Post, Put, Body, Del } from '@midwayjs/core';
+import { Controller, Inject, Get, Param, Post, Put, Body, Del, Query } from '@midwayjs/core';
 import { Context } from '@midwayjs/koa';
 import { Trade } from '../entity/trade';
+import { CustomHttpError } from '../error/custom.error';
 
 @Controller('/api/trade')
 export class TradeController {
@@ -8,20 +9,32 @@ export class TradeController {
   ctx: Context;
 
   @Get('/')
-  index() {
-    return Trade.findAll({
-      where: { state: true }
-    })
+  async index(@Query('state') state: any) {
+    const where: { [key: string]: any } = {}
+    !state && (where['state'] = true)
+    return Trade.findAll({ where })
   }
 
-  @Put('/:id/edit')
-  edit(@Param() id: any) {
-    console.log(id)
+  @Put('/:id')
+  async edit(@Param('id') id: number, @Body() b: any) {
+    const me = this.ctx.adminer
+    if (me.roleId == 3) throw new CustomHttpError('您无权限')
+    await Trade.update(b, { where: { id } })
+    return id
   }
 
   @Post('/store')
-  store(@Body() b: any) { }
+  async store(@Body() b: any) {
+    const me = this.ctx.adminer
+    if (me.roleId == 3) throw new CustomHttpError('您无权限')
+    await Trade.create(b)
+    return 'ok'
+  }
 
-  @Del('/del')
-  destroy() { }
+  @Del('/:id')
+  destroy(@Param('id') id: number) {
+    const me = this.ctx.adminer
+    if (me.roleId == 3) throw new CustomHttpError('您无权限')
+    Trade.destroy({ where: { id } })
+  }
 }
